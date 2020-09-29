49438b05bcc3154f00029733193bd506ec63e835
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2020-09-29 18:27:24 -0300

    utilizando styled components

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
-/**
- * Como utiliza Typescript é melhor declarar a variavel como constante pois
- * assim fica mais fácil de adicionar tipagem.
- * No caso vão ser sempre do tipo React.FC (Functional Component)
- */
+import {Title} from './style';
+
 
 const Dashboard: React.FC = () => {
   return (
-    <h1>Dashboard</h1>
+    <Title>Explore repositórios no Github.</Title>
   );
 }
 
 export default Dashboard;
 
+
+/**
+ * Como utiliza Typescript é melhor declarar a variavel como constante pois
+ * assim fica mais fácil de adicionar tipagem.
+ * No caso vão ser sempre do tipo React.FC (Functional Component)
+ */
