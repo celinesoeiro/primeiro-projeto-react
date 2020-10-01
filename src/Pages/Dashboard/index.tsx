74951aee8b37ab341afe3f18ee6c032729e398b2
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2020-10-01 19:42:25 -0300

    conectando com a api

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,54 +1,67 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/images/logo.svg';
 
 import { Title, Form, Repositories } from './style';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  /** FUNCTIONS */
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
+    setNewRepo('');
+  }
+
+
   return (
     <>
       <img src={logo} alt='github explorer'/>
+
       <Title>Explore repositórios no Github.</Title>
-      <Form>
-        <input placeholder="Digite o nome do repositório."/>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório."
+        />
         <button type="submit">Pesquisar</button>
       </Form>
       <Repositories>
-        <a href="teste">
+        {repositories.map(repo => (
+        <a key={repo.full_name} href="teste">
           <img
-            src="https://avatars1.githubusercontent.com/u/52112166?s=460&u=a3c9425787c98164fabd55c59c8dc0b8a84e0da0&v=4"
-            alt="avatar"
+            src={repo.owner.avatar_url}
+            alt={repo.owner.login}
           />
           <div>
-            <strong>celinesoeiro/cat_dog_classifier</strong>
-            <p>Classificador de gatos e cachorros construído com CNN.</p>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
           </div>
           <FiChevronRight size={20}/>
         </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/52112166?s=460&u=a3c9425787c98164fabd55c59c8dc0b8a84e0da0&v=4"
-            alt="avatar"
-          />
-          <div>
-            <strong>celinesoeiro/cat_dog_classifier</strong>
-            <p>Classificador de gatos e cachorros construído com CNN.</p>
-          </div>
-          <FiChevronRight size={20}/>
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/52112166?s=460&u=a3c9425787c98164fabd55c59c8dc0b8a84e0da0&v=4"
-            alt="avatar"
-          />
-          <div>
-            <strong>celinesoeiro/cat_dog_classifier</strong>
-            <p>Classificador de gatos e cachorros construído com CNN.</p>
-          </div>
-          <FiChevronRight size={20}/>
-        </a>
+        ))}
       </Repositories>
 
     </>
