a6327cb3ffd6722e3ef9e5f4167fca9e138c3990
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2020-10-01 16:59:52 -0300

    estilizando o dashboard

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import {Title} from './style';
+import logo from '../../assets/images/logo.svg';
+
+import { Title, Form, Repositories } from './style';
 
 
 const Dashboard: React.FC = () => {
   return (
-    <Title>Explore repositórios no Github.</Title>
+    <>
+      <img src={logo} alt='github explorer'/>
+      <Title>Explore repositórios no Github.</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório."/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/52112166?s=460&u=a3c9425787c98164fabd55c59c8dc0b8a84e0da0&v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>celinesoeiro/cat_dog_classifier</strong>
+            <p>Classificador de gatos e cachorros construído com CNN.</p>
+          </div>
+          <FiChevronRight size={20}/>
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/52112166?s=460&u=a3c9425787c98164fabd55c59c8dc0b8a84e0da0&v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>celinesoeiro/cat_dog_classifier</strong>
+            <p>Classificador de gatos e cachorros construído com CNN.</p>
+          </div>
+          <FiChevronRight size={20}/>
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/52112166?s=460&u=a3c9425787c98164fabd55c59c8dc0b8a84e0da0&v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>celinesoeiro/cat_dog_classifier</strong>
+            <p>Classificador de gatos e cachorros construído com CNN.</p>
+          </div>
+          <FiChevronRight size={20}/>
+        </a>
+      </Repositories>
+
+    </>
   );
 }
 
